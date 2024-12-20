a34de4a490e6c03a09821fa239b8bd40c7f3eacb
developernilesh <nileshmukherjee2017@gmail.com>
2024-12-20 23:57:46 +0530

    Search page added

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -23,7 +23,23 @@ const Search = () => {
     fetchSearchResults();
   }, [filter]);
   
-  return <div>Search</div>;
+  return (
+    <div className="my-4">
+      <h2 className="text-5xl pb-3 font-extrabold">{query}</h2>
+      <FilterGif alignLeft={true} />
+      {searchResults.length > 0 ? (
+        <div className="columns-2 md:columns-3 lg:columns-4 gap-2">
+          {searchResults.map((gif) => (
+            <Gif gif={gif} key={gif.id} />
+          ))}
+        </div>
+      ) : (
+        <span>
+          No GIFs found for {query}. Try searching for Stickers instead?
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default Search;
